6c90d90a7d21fe66202fcbddeefe5d35d877412c
Kevin McAllister <kevinryan@videotron.ca>
2020-10-24 17:22:57 -0400

    Added Programming Guidelines.txt
    Created initial Unity Project Layout:
    - Added Scenes for each stage
    - Added Folders for separate script categories
    - Added Example scripts and empty templates

---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ----------------------------------------------------------------------------------------------------
+//	Description: Class determining basic entity behaviour and statistics
+//	Contributors:
+//	Endpoints:
+// ----------------------------------------------------------------------------------------------------
+
+public class Entity : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start() {
+		
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
