86526334cdf8c6d5478dc1c608068faeb709a5a8
Jordan Goulet <jordang1995@hotmail.com>
2020-10-28 15:25:39 -0400

    Basic test level and player movement
    I made a few prefab world objects with static rigid bodies and placed some of them in the test scene to test player collisions.
    
    I made a player prefab.
    
    I made an InputManager object to handle input (I don't know if thats the way we should do it or not though).
    
    I handled WASD inputs to move the player through its script which is found with "FindObjectOfType" (I don't know if this is good or no).
    
    I gave the Entity a movement_speed variable and made a move function which can be used to move player or enemies depending on their speed.
    
    The player is currently able to move and collide with the world objects.

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -10,13 +10,21 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+	float move_speed = 1.0f;
+
 	// Start is called before the first frame update
 	void Start() {
-		
 	}
 
 	// Update is called once per frame
 	void Update() {
 		
 	}
+
+    public void move(float x, float y)
+    {
+		x = x * Time.deltaTime * move_speed + gameObject.transform.position.x;
+		y = y * Time.deltaTime * move_speed + gameObject.transform.position.y;
+		gameObject.transform.position = new Vector3(x, y, 0.0f);
+    }
 }
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        if (player == null)
+        {
+            print("Error: Player not found.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            player.move(0.0f, 1.0f);
+        }
+        if (Input.GetKey("a"))
+        {
+            player.move(-1.0f, 0.0f);
+        }
+        if (Input.GetKey("s"))
+        {
+            player.move(0.0f, -1.0f);
+        }
+        if (Input.GetKey("d"))
+        {
+            player.move(1.0f, 0.0f);
+        }
+    }
+}
