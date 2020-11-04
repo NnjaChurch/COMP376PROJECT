b615d2bb07b961161e2c50edc9edd8f33a0a78e2
Rubiat Zaman <rubiat.zaman@gmail.com>
2020-11-03 19:47:49 -0500

    Main Menu UI
    Really lacking creativity though

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,21 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // ----------------------------------------------------------------------------------------------------
 //	Description: Class for the main menu behaviour
-//	Contributors:
+//	Contributors: Rubiat
 //	Endpoints:
 // ----------------------------------------------------------------------------------------------------
 
 public class MainMenu : MonoBehaviour {
-	// Start is called before the first frame update
-	void Start() {
-
-	}
-
-	// Update is called once per frame
-	void Update() {
-
-	}
+    // Start is called before the first frame update
+    public void ButtonOptionClick(string option)
+    {
+        if (option == "New Game")
+        {
+            SceneManager.LoadScene("Test Scene");
+            // TODO change this to the actual scene instead of Test Scene
+        }
+        else if (option == "Load Game")
+        {
+            // TODO have to implement this
+        }
+        else if (option == "Exit")
+        {
+            Application.Quit();
+        }
+    }
 }
