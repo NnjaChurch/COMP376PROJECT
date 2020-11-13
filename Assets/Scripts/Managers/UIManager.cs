79330510d46cd486bd758391cef552fa47355824
Kevin McAllister <kevinryan@videotron.ca>
2020-11-12 21:46:10 -0500

    - Addition of new managers
    - Created connection points in managers for their children classes and communicating managers

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	// UI Class References
+	[SerializeField] CharacterUI UI_character;
+	[SerializeField] InventoryUI UI_inventory;
+	[SerializeField] GameUI UI_game;
+	[SerializeField] SkillsUI UI_skills;
+	[SerializeField] UpgradeUI UI_upgrade;
+
+	// Menu Class References
+	[SerializeField] LootMenu menu_loot;
+	[SerializeField] PauseMenu menu_pause;
+
+	// Manager Class References
+	[SerializeField] StatsManager manager_stats;
+	[SerializeField] InventoryManager manager_inventory;
+	//[SerializeField] LootManager manager_loot;	TODO: Hook up LootManager once implemented
+
+
+}
