cb0066bbabf08a85be216bff42451379c9a161a7
Jordan Goulet <jordang1995@hotmail.com>
2020-12-01 14:01:55 -0500

    More lighting

---
+++ Assets/Scripts/Stage/CopLights.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class CopLights : MonoBehaviour
+{
+    public Light2D light;
+    public int offset;
+    public int frequency;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = (Mathf.Sin((frequency * Time.time) - ((offset/360) * Mathf.PI)) + 1) / 4;
+    }
+}
