4489e62e86ecfedaf550cacbd226b4111e3434e1
Kevin McAllister <kevinryan@videotron.ca>
2020-12-08 17:21:11 -0500

    - Moved SkillUI and UpgradeUI into SafeZoneUI folder

---
+++ Assets/Scripts/UI/SafeZoneUIs/SkillsUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ----------------------------------------------------------------------------------------------------
+//	Description: Class for the skills upgrade User Interface
+//	Contributors:
+//	Endpoints:
+// ----------------------------------------------------------------------------------------------------
+
+public class SkillsUI : MonoBehaviour {
+
+	[SerializeField] UIManager manager_ui;
+
+	public void Initialize() {
+		Debug.Log("Initializing SkillsUI...");
+		// TODO: Populate Skills UI
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	public void UpgradeSkill(int skill_number) {
+		manager_ui.UpgradeSkill(skill_number);
+	}
+
+	public void UpdateSkillsUI(int skill_points, List<Skill> skills_list) {
+		// TODO: Use skill information to upate UI
+	}
+
+
+}
